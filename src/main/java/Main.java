5e3675af3b3e9877be2bd8d60fb8fa527ac27058
JDELGADILP-DV11\jp <juan.pablo.delgadillo.peredo@gmail.com>
2020-07-12 00:02:40 -0400

    Práctica 3 - Punto 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Main {
+    public static void main(String[] args){
+        System.out.println("-------Práctica 3 - Punto 3-------------");
+
+        ArrayList<String> test3= new ArrayList<>();
+        test3.add("Bolivia");
+        test3.add("Argentina");
+        test3.add("Argelia");
+        test3.add("Italia");
+        test3.add("Australia");
+        test3.stream()
+                .filter(s -> s.length()%2 == 0)
+                .forEach(System.out::println);
+    }
 }
