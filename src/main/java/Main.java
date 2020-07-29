3cd51bc87c0acdcec0b6ca0722a47ad0d5d8d007
JDELGADILP-DV11\jp <juan.pablo.delgadillo.peredo@gmail.com>
2020-07-28 22:32:19 -0400

    Práctica 5 point 1,2 and 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("----------Práctica 5-------------");
+        System.out.println("----------Práctica 5 point 1-------------");
 
         List<Pedido> pedidoList = new ArrayList<>();
         pedidoList.add(new Pedido("ped1","normal","Juan Perez","2020-05-05"));
@@ -14,5 +14,14 @@ public class Main {
 
         // select cliente from Pedido;
         pedidoList.stream().map(Pedido::getCliente).forEach(value -> System.out.println(value));
+
+        System.out.println("----------Práctica 5 point 2-------------");
+        //Select cliente from Pedido where tipoPedido = “= normal“
+        pedidoList.stream().filter(p ->p.getTipoPedido() == "normal").map(Pedido::getCliente).forEach(value -> System.out.println(value));
+
+        System.out.println("----------Práctica 5 point 3-------------");
+        //Select codPedido from Pedido where fecha = "2020-05-05"
+        pedidoList.stream().filter(p ->p.getFecha() == "2020-05-05").map(Pedido::getCodPedido).forEach(value -> System.out.println(value));
+
     }
 }
